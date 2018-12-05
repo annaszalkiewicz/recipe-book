62c5387cf6ee48013521ef96944673b78ae10ce6
annaszalkiewicz <anna@mywebgraphicdesign.com>
2018-12-06 00:00:58 +0100

    Create shopping-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipeBookComponent } from './recipe-book/recipe-book.component';
+import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipeBookComponent
+    RecipeBookComponent,
+    RecipeListComponent,
+    ShoppingListComponent
   ],
   imports: [
     BrowserModule

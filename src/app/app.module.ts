99b736bd967dbf07475f3599f8889cedab0491ad
annaszalkiewicz <anna@mywebgraphicdesign.com>
2018-12-27 00:02:33 +0100

    Import FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -24,7 +25,8 @@ import { RecipeItemComponent } from './recipe-book/recipe-list/recipe-item/recip
     RecipeItemComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

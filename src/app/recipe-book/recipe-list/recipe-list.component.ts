59ccb523cce8f4c967c515ae1c9e4832b876644d
annaszalkiewicz <anna@mywebgraphicdesign.com>
2018-12-26 22:51:24 +0100

    Add onSelect() that emit data to parent component

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
 import { Recipe } from './../recipe.model';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,15 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+  @Output() selected = new EventEmitter<string>();
+
   recipes: Recipe[] = [
-    new Recipe('Test Recipe Name', 'This is test recipe description', 'https://images.pexels.com/photos/806363/pexels-photo-806363.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260'),
-    new Recipe('Test Recipe Name', 'This is test recipe description', 'https://images.pexels.com/photos/806363/pexels-photo-806363.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260'),
-    new Recipe('Test Recipe Name', 'This is test recipe description', 'https://images.pexels.com/photos/806363/pexels-photo-806363.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260'),
+    new Recipe('Test Recipe Name 1', 'This is test recipe description', 'https://images.pexels.com/photos/806363/pexels-photo-806363.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260'),
+    new Recipe('Test Recipe Name 2 ', 'This is test recipe description', 'https://images.pexels.com/photos/806363/pexels-photo-806363.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260'),
+    new Recipe('Test Recipe Name 3', 'This is test recipe description', 'https://images.pexels.com/photos/806363/pexels-photo-806363.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260'),
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelect(currentRecipe: string) {
+    this.selected.emit(currentRecipe);
   }
 
 }

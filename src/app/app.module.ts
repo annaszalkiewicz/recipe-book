801b13396ff294bf0f31f4a025942ee315fcc094
annaszalkiewicz <anna@mywebgraphicdesign.com>
2018-12-26 22:12:10 +0100

    Change components structure
    Moved recipe-details folder to recipe-book folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { HeaderComponent } from './header/header.component';
 import { RecipeBookComponent } from './recipe-book/recipe-book.component';
 import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { RecipeDetailsComponent } from './recipe-book/recipe-list/recipe-details/recipe-details.component';
+import { RecipeDetailsComponent } from './recipe-book/recipe-details/recipe-details.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { FooterComponent } from './footer/footer.component';
 

700c5cd31a9f48ee18b795e0de1f83220af1b01d
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-01-14 00:23:41 +0100

    Create recipes array & create getRecipes method
    Returns copy of all recipes in array

--- src/app/recipe-book/recipe.service.ts
+++ src/app/recipe-book/recipe.service.ts
@@ -1,9 +1,33 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RecipeService {
+export class RecipeService implements OnInit {
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Test Recipe Name 1',
+      'This is test recipe description',
+      'https://images.pexels.com/photos/806363/pexels-photo-806363.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260'
+    ),
+    new Recipe(
+      'Test Recipe Name 2 ',
+      'This is test recipe description',
+      'https://images.pexels.com/photos/806363/pexels-photo-806363.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260'
+    ),
+    new Recipe(
+      'Test Recipe Name 3',
+      'This is test recipe description',
+      'https://images.pexels.com/photos/806363/pexels-photo-806363.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260'
+    )
+  ];
 
-  constructor() { }
+  constructor() {}
+
+  ngOnInit() {}
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
 }

b2827c696aaf54822ea436ecc01fce79a7f8e51a
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-01-14 21:40:25 +0100

    Add properties and methods to ShoppingList service

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,27 @@
-import { Injectable } from '@angular/core';
+import { Ingredient } from './../shared/ingredient.model';
+import { Injectable, EventEmitter } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingListService {
 
+  changedIngredients = new EventEmitter<Ingredient[]>();
+
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Fleur', 1)
+
+  ];
+
   constructor() { }
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.changedIngredients.emit(this.ingredients.slice());
+  }
 }

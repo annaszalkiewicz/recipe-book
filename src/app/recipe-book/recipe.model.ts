2b5ee061a2aaaf193c6b2d071463fa0ef2b8a006
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-01-20 21:34:38 +0100

    Add id of type string to model

--- src/app/recipe-book/recipe.model.ts
+++ src/app/recipe-book/recipe.model.ts
@@ -1,12 +1,14 @@
 import { Ingredient } from './../shared/ingredient.model';
 
 export class Recipe {
+  public id: string;
   public name: string;
   public description: string;
   public imageUrl: string;
   public ingredients: Ingredient[];
 
-  constructor(name: string, desc: string, image: string, ingredients: Ingredient[]) {
+  constructor(id: string, name: string, desc: string, image: string, ingredients: Ingredient[]) {
+    this.id = id;
     this.name = name;
     this.description = desc;
     this.imageUrl = image;

93335383431acb133a390ba3915d26acd791d948
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-01-20 22:03:10 +0100

    Add new child path to /recipes
    This path display recipe-start component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { RecipeStartComponent } from './recipe-book/recipe-start/recipe-start.component';
+import { RecipeDetailsComponent } from './recipe-book/recipe-details/recipe-details.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipeBookComponent } from './recipe-book/recipe-book.component';
 import { AppComponent } from './app.component';
@@ -12,7 +14,17 @@ const appRoutes: Routes = [
   },
   {
     path: 'recipes',
-    component: RecipeBookComponent
+    component: RecipeBookComponent,
+    children: [
+      {
+        path: '',
+        component: RecipeStartComponent
+      },
+      {
+        path: ':id',
+        component: RecipeDetailsComponent
+      }
+    ]
   },
   {
     path: 'shopping-list',

d187e79e1f6c22cec112e375a538f14c21068e4b
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-02-14 23:56:00 +0100

    Add ingredients on recipe edit form

--- src/app/recipe-book/recipe-edit/recipe-edit.component.ts
+++ src/app/recipe-book/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
 
 import { RecipeService } from './../recipe.service';
 
@@ -30,6 +30,7 @@ export class RecipeEditComponent implements OnInit {
     let recipeName = '';
     let recipeImageUrl = '';
     let recipeDescription = '';
+    const recipeIngredients = new FormArray([]);
 
     if (this.editMode) {
       const recipe = this.recipeService.getRecipe(this.id);
@@ -37,12 +38,27 @@ export class RecipeEditComponent implements OnInit {
       recipeName = recipe.name;
       recipeImageUrl = recipe.imageUrl;
       recipeDescription = recipe.description;
+      if (recipe['ingredients']) {
+        for (const ingredient of recipe.ingredients) {
+          recipeIngredients.push(
+            new FormGroup({
+              'ingredientName': new FormControl(ingredient.name),
+              'ingredientAmount': new FormControl(ingredient.amount)
+            })
+          )
+        }
+      }
     }
     this.recipeForm = new FormGroup({
       name: new FormControl(recipeName),
       imageUrl: new FormControl(recipeImageUrl),
       description: new FormControl(recipeDescription),
+      ingredients: recipeIngredients
     });
+  }
+
+  getControls() {
+    return (<FormArray>this.recipeForm.get('ingredients')).controls;
   }
 
   onSubmit() {

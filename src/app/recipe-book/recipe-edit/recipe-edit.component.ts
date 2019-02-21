76ced992627de091757567daa8ab4999948d2773
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-02-21 23:56:34 +0100

    Navigate to parent route on form submit

--- src/app/recipe-book/recipe-edit/recipe-edit.component.ts
+++ src/app/recipe-book/recipe-edit/recipe-edit.component.ts
@@ -68,13 +68,14 @@ export class RecipeEditComponent implements OnInit {
   }
 
   onSubmit() {
-    console.log(this.recipeForm);
 
     if (this.editMode) {
       this.recipeService.updateRecipe(this.id, this.recipeForm.value);
     } else {
       this.recipeService.addRecipe(this.recipeForm.value);
     }
+
+    this.onCancel();
   }
 
   addIngredient() {

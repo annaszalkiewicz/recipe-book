e2d84dbb067b316b9500c2d4b37348f6d0ae649b
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-06-28 23:18:00 +0200

    Fix PUT http request

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,19 +1,21 @@
-import { RecipeService } from './../recipe-book/recipe.service';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+
+import { RecipeService } from './../recipe-book/recipe.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataStorageService {
-
-  constructor(private http: HttpClient, private recipeService: RecipeService) { }
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
 
   storeRecipe() {
     const recipes = this.recipeService.getRecipes();
 
-    this.http.put('https://recipe-book-de100.firebaseio.com/', recipes).subscribe(recipe => {
-      console.log(recipe);
-    });
+    this.http
+      .put('https://recipe-book-de100.firebaseio.com/recipes.json', recipes)
+      .subscribe(recipe => {
+        console.log(recipe);
+      });
   }
 }

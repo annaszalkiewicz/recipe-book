b3ff5a14a115157717d53fb6b43f5d3859600703
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-06-28 22:25:41 +0200

    Create storeRecipe method that uses PUT http request

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,9 +1,19 @@
+import { RecipeService } from './../recipe-book/recipe.service';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataStorageService {
 
-  constructor() { }
+  constructor(private http: HttpClient, private recipeService: RecipeService) { }
+
+  storeRecipe() {
+    const recipes = this.recipeService.getRecipes();
+
+    this.http.put('https://recipe-book-de100.firebaseio.com/', recipes).subscribe(recipe => {
+      console.log(recipe);
+    });
+  }
 }
